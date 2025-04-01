15beeded5b061a8ffc6ff7a80a55a1b4f40b4eaa
takumiooooooooigawa <takumi.oigawa@gmail.com>
2025-04-01 21:05:47 +0900

    app init

---
+++ manage-finance/manage-finance/manage_financeApp.swift
@@ -0,0 +1,17 @@
+//
+//  manage_financeApp.swift
+//  manage-finance
+//
+//  Created by Takumi Oigawa on 2025/04/01.
+//
+
+import SwiftUI
+
+@main
+struct manage_financeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
